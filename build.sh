82bdd78d878085c74c346244ed7853a457f52de9
Jack Dunham <jack.dunham@gmail.com>
2025-02-03 07:50:30 -0800

    build session-server

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+# TARGET linux/amd64/v4
+#docker build -f Dockerfile.webserver -t linkwebserver:latest .
+docker buildx build -f Dockerfile.webserver --platform linux/amd64 -t session-server:latest --load .
 
-docker build -f Dockerfile.webserver -t linkwebserver:latest .
-docker run -p 8082:8082 linkwebserver:1
+#docker run -p 8082:8082 linkwebserver:1
+#docker images
+#docker save -o session_server.tar 5d1332269f0a
 
---
+++ cmd/http/ssh_to_lightsail.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ssh -i ~/Downloads/LightsailDefaultKey-us-west-2.pem  ec2-user@52.88.89.145
+
